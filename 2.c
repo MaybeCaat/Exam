7b0f09dfe3ccc03d1a5b572ad08dc3de0273cda2
MaybeCaat <iemm@rambler.ru>
2023-06-02 10:56:53 +0300

    task 2 completed

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int sumFoo(int n) {
+    int result = 0;
+    for (int i=1; i<=n; i++) {
+        result += (int) pow(i, 4);
+    }
+    return result;
+}
+
+int main() {
+    int n = 3;
+    printf("%d", sumFoo(n));
+    return 0;
+}
